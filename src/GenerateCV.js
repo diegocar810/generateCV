40a8208abaa232c0844915ed7efd1c67f8a6dff6
Juan Diego Cardona <juan.diego.cardona@accenture.com>
2022-05-05 23:41:32 -0500

    install tailwind

---
+++ src/GenerateCV.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const GenerateCV = () => {
+    return (
+        <>
+            <div>GenerateCV</div>
+            <h1 className="text-blue-400 font-extrabold">Hello World!</h1>
+            <h1 className="text-3xl font-bold underline">
+                Hello world! 
+            </h1>
+        </>
+    )
+}
